300b901c18159436295ea028108a421a099ed58c
Eloy Robillard <eloy.robillard@ibm.com>
2023-07-04 14:42:19 +0200

    Make package installable using NPM

---
+++ npm/preinstall.js
@@ -0,0 +1,36 @@
+const { mkdirSync, existsSync, copyFileSync } = require("fs");
+const { rm } = require("fs/promises");
+
+const { platform, arch } = process;
+
+const TARGETS = {
+  win32: {
+    x64: "x86_64-pc-windows-msvc",
+    arm64: "aarch64-pc-windows-msvc",
+  },
+  darwin: {
+    x64: "x86_64-apple-darwin",
+    arm64: "aarch64-apple-darwin",
+  },
+  linux: {
+    x64: "x86_64-unknown-linux-gnu",
+    arm64: "aarch64-unknown-linux-gnu",
+  },
+}
+
+const target = TARGETS[platform][arch];
+
+const cwd = process.cwd();
+
+const binaryPath = `${cwd}/builds/${target}/debs`;
+
+if (!existsSync(`${cwd}/bin`)) {
+  mkdirSync(`${cwd}/bin`,)
+}
+
+copyFileSync(binaryPath, `${cwd}/bin/debs`)
+
+rm(`${cwd}/src`, { recursive: true, force: true })
+rm(`${cwd}/test-assets`, { recursive: true, force: true })
+rm(`${cwd}/script`, { recursive: true, force: true })
+rm(`${cwd}/DEV_GUIDE.md`, { recursive: true, force: true })
